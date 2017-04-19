eeb60c45719298d9ebd1e9a34f82d81e48d515e6
Abhinav Das <abhinav.adtechs@gmail.com>
2017-04-19 23:30:52 +0530

    other networking setups

---
+++ app/src/main/java/io/cse325/businesstoday/BaseActivity.java
@@ -0,0 +1,15 @@
+package io.cse325.businesstoday;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
